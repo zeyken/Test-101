86f05ef8a20b70d8849466f19b47f89c6780f5bb
zeyken <karmakatsukie@gmail.com>
2024-09-16 21:50:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Live on Twitch", "Playing VALORANT"];
+const statusMessages = ["Hotdog", "Inaantok"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
