282f83dce2044a066e0cb2a9aafdc387131611fd
zeyken <karmakatsukie@gmail.com>
2024-09-16 23:14:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Hotdog", "Inaantok"];
+const statusMessages = ["discord.gg/pixiedust"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
